0f8e776eb091a7e4ca8c353aaaf23ef1f0006f89
rust-le <mateusz@rustle.pl>
2023-02-17 19:26:13 +0100

    changed return type of fn parse_text to Result<Option<Color>>

--- tsi/src/main.rs
+++ tsi/src/main.rs
@@ -1,13 +1,16 @@
 use std::io;
+
+use crate::parsing::parse_text;
 mod parsing;
 
 fn main() {
     let mut text: String = String::new();
     while io::stdin().read_line(&mut text).is_ok() {
         let trimmed = text.trim_end();
-        let value: parsing::Color = parsing::parse_text(&trimmed).unwrap();
+        if let Ok(Some(value)) = parse_text(trimmed) {
+            println!("{:?}", value);
+        }
         println!("Parsed text: {}", trimmed);
-        println!("{:?}", value);
         text.clear();
     }
 }
--- tsi/src/parsing.rs
+++ tsi/src/parsing.rs
@@ -1,32 +1,62 @@
 use regex::Regex;
 use std::error::Error;
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Color {
     pub red: bool,
     pub green: bool,
     pub yellow: bool,
 }
-pub fn parse_text(text: &str) -> Result<Color, Box<dyn Error>> {
-    unimplemented!()
+impl Color {
+    fn new() -> Self {
+        Color {
+            red: false,
+            green: false,
+            yellow: false,
+        }
+    }
+}
+pub fn parse_text(text: &str) -> Result<Option<Color>, Box<dyn Error>> {
+    let re = Regex::new(r"^\[(Running|Finished running).+]$").unwrap();
+    if re.is_match(text) {
+        let c = match text {
+            "[Finished running. Exit status: 0]" => Some(Color::new()),
+            _ => Some(Color::new()),
+        };
+        Ok(c)
+    } else {
+        Ok(None)
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
-    fn test_green_messages() {
+    fn test_parse_text() {
         // arrange
-        let messages = ["[Finished running. Exit status: 0]"];
-        let should_be_green: Color = Color {
-            red: false,
-            green: true,
-            yellow: false,
+        let blank: Color = Color::new();
+        let red: Color = Color { red: true, ..blank };
+        let yellow: Color = Color {
+            yellow: true,
+            ..blank
         };
+        let green: Color = Color {
+            green: true,
+            ..blank
+        };
+        let messages = vec![
+            ("[Finished running. Exit status: 0]", &green),
+            ("[Running 'cargo check']", &yellow),
+            ("[Finished running. Exit status: 101]", &red),
+            ("[Running 'cargo test && cargo build']", &yellow),
+        ];
         // act
-        // assert
-        messages.into_iter().for_each(|message| {
+        messages.into_iter().for_each(|(message, color)| {
+            // assert
             assert!(parse_text(message).is_ok());
-            assert_eq!(parse_text(message).unwrap(), should_be_green);
+            if let Ok(Some(c)) = parse_text(message) {
+                assert_eq!(&c, color);
+            }
         });
     }
 }
