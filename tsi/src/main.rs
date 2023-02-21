f0b62481cb2b89fee2226b87edf1cc370fcaf626
rust-le <mateusz@rustle.pl>
2023-02-21 12:33:12 +0100

    horrible mess

--- tsi/src/main.rs
+++ tsi/src/main.rs
@@ -1,6 +1,5 @@
 use std::io;
 extern crate hidapi;
-
 use crate::parsing::parse_text;
 use hidapi::HidApi;
 mod parsing;
@@ -10,6 +9,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for device in api.device_list() {
         println!("{:04x}:{:04x}", device.vendor_id(),device.product_id());
     }
+
     let mut text: String = String::new();
     while io::stdin().read_line(&mut text).is_ok() {
         let trimmed = text.trim_end();
